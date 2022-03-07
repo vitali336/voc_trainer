f2ab7f84ed96186b925f461c95691629c3584bc2
Vitali <vitali.altuchow@alumni.fh-aachen.de>
2022-03-07 09:23:47 +0100

    serializing works well

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,48 @@
 package com.company;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("privet");
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("Apfel");
+        words.add("Tisch");
+        words.add("Mond");
+
+        String filename = "words.ser";
+
+        // save the object to file
+        FileOutputStream fos = null;
+        ObjectOutputStream out = null;
+        try {
+            fos = new FileOutputStream(filename);
+            out = new ObjectOutputStream(fos);
+            out.writeObject(words);
+
+            out.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        // read the object from file
+        // save the object to file
+        FileInputStream fis = null;
+        ObjectInputStream in = null;
+        try {
+            fis = new FileInputStream(filename);
+            in = new ObjectInputStream(fis);
+            words = (ArrayList<String>) in.readObject();
+            in.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println(words);
     }
 }
