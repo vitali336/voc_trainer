c52a2bcf5c616b797405772ae2bf0f5b7c32665d
Vitali <vitali.altuchow@alumni.fh-aachen.de>
2022-03-11 21:46:56 +0100

    added functions readlist and writeList to main for serialisation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,25 +5,49 @@ import java.util.ArrayList;
 
 public class Main {
 
+    public static VocabularyList readList(String filename){
+        FileInputStream fis = null;
+        ObjectInputStream in = null;
+        try {
+            fis = new FileInputStream(filename);
+            in = new ObjectInputStream(fis);
+            VocabularyList vocabularyList = (VocabularyList) in.readObject();
+            in.close();
+            return vocabularyList;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void writeList(String filename, VocabularyList vocabularyList){
+        // save the object to file
+        FileOutputStream fos = null;
+        ObjectOutputStream out = null;
+        try {
+            fos = new FileOutputStream(filename);
+            out = new ObjectOutputStream(fos);
+            out.writeObject(vocabularyList);
+            out.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
 
-        VocabularyList vocabularyList = new VocabularyList();
-
-        //vocabularyList.addVocabulary(new Vocabulary("Tisch", "table"));
-        //vocabularyList.addVocabulary(new Vocabulary("Apfel", "apple"));
-        //vocabularyList.addVocabulary(new Vocabulary("Hund", "dog"));
-
+        VocabularyList vocabularyList;
         String filename = "vocabularies.ser";
 
         String input = "";
-
         String input_de = "";
         String input_en = "";
 
         java.io.BufferedReader in_ = new BufferedReader(new InputStreamReader(System.in));
 
         do {
+            vocabularyList = readList(filename);
+
             System.out.print("Show Vocabulary-List (1) " + '\n'
                 + "Add Vocabulary (2)" + '\n');
             try{
@@ -34,65 +58,22 @@ public class Main {
 
             if(input.equals("1"))
             {
+                System.out.println(vocabularyList);
 
-                FileInputStream fis = null;
-                ObjectInputStream in = null;
-                try {
-                    fis = new FileInputStream(filename);
-                    in = new ObjectInputStream(fis);
-                    vocabularyList = (VocabularyList) in.readObject();
-                    System.out.println(vocabularyList);
-                    in.close();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
             }else if(input.equals("2")){
-
-
-                FileInputStream fis = null;
-                ObjectInputStream in = null;
-                try {
-                    fis = new FileInputStream(filename);
-                    in = new ObjectInputStream(fis);
-                    vocabularyList = (VocabularyList) in.readObject();
-                    in.close();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-
                 try{
                     System.out.print("Deutsch: ");
                     input_de = in_.readLine();
                     System.out.print("Englisch: ");
                     input_en = in_.readLine();
-
                 }catch (Exception e){
                     e.printStackTrace();
                 }
 
                 vocabularyList.addVocabulary(new Vocabulary(input_de, input_en));
-
-                // save the object to file
-                FileOutputStream fos = null;
-                ObjectOutputStream out = null;
-                try {
-                    fos = new FileOutputStream(filename);
-                    out = new ObjectOutputStream(fos);
-                    out.writeObject(vocabularyList);
-
-                    out.close();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
+                writeList(filename, vocabularyList);
             }
+
         }while (!input.equals("close"));
-
-
-
-
-
-        // read the object from file
-        // save the object to file
-
     }
 }
